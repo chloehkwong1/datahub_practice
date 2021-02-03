b14efaa467ff9f16f8d181819ac42c11ae9c5d0e
Chloe <chloe.wong@madetech.com>
2021-02-03 11:32:22 +0000

    added model to upload csv file

--- datahub/admin.py
+++ datahub/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BEDTemplate, Csv
+
+admin.site.register(BEDTemplate)
+admin.site.register(Csv)
--- datahub/models.py
+++ datahub/models.py
@@ -13,3 +13,15 @@ class BEDTemplate(models.Model):
     city_or_town = models.CharField(max_length=200)
     attending_organisations = models.TextField()
     attending_contacts = models.TextField()
+
+    def __str__(self):
+        return f"{self.interaction_title} happened on {self.interaction_date}. Contact {self.lead_minister_or_official} for more information."
+
+class Csv(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    file_name = models.FileField(upload_to='csvs')
+    uploaded = models.DateTimeField(auto_now_add=True)
+    activated =models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"File id: {self.id}"
