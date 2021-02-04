93e2127ceeb1c9189535edc4c0c781f8af4c185a
Chloe <chloe.wong@madetech.com>
2021-02-04 09:57:04 +0000

    able to add information from csv file to the database

--- datahub/views.py
+++ datahub/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import CsvModelForm
+from .models import Csv, BEDTemplate
+from dateutil.parser import parse
+
+import csv
 
 def index(request):
     return HttpResponse('Welcome to DIT Datahub.')
@@ -11,4 +15,24 @@ def upload_file_view(request):
     if form.is_valid():
         form.save()
         form = CsvModelForm()
+        obj = Csv.objects.get(activated=False)
+        with open(obj.file_name.path, 'r') as f:
+            reader = csv.reader(f)
+            for i, row in enumerate(reader):
+                if i==0:
+                    pass
+                else:
+                    BEDTemplate.objects.create(
+                        interaction_title = row[0],
+                        interaction_date = parse(row[1]),
+                        lead_minister_or_official = row[2],
+                        type = row[3],
+                        country = row[4],
+                        region = row[5],
+                        city_or_town = row[6],
+                        attending_organisations = row[7],
+                        attending_contacts = row[8],
+                    )
+                obj.activated = True
+                obj.save()
     return render(request, 'csvs/upload.html', {'form': form})
