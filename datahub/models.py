399f94e274af12cd1df9eecf1caf245056a2615f
Chloe <chloe.wong@madetech.com>
2021-02-08 12:54:43 +0000

    Added choices for types of meeting and country dropdown field

--- datahub/models.py
+++ datahub/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django_countries.fields import CountryField
 import uuid
 
 
@@ -7,15 +8,33 @@ class BedTemplate(models.Model):
     interaction_title = models.CharField(max_length=200, unique=True)
     interaction_date = models.DateField()
     lead_minister_or_official = models.EmailField()
-    type = models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
-    region = models.CharField(max_length=200)
-    city_or_town = models.CharField(max_length=200)
+    type = models.CharField(
+        choices=[('bilateral_meeting', 'Bilateral Meeting'),
+                 ('brush_by', 'Brush By'),
+                 ('conference', 'Conference'),
+                 ('email', 'Email'),
+                 ('forum', 'Forum'),
+                 ('letter', 'Letter'),
+                 ('multilateral_meeting', 'Multilateral Meeting'),
+                 ('phone_call', 'Phone Call'),
+                 ('reception', 'Reception'),
+                 ('roadshow', 'Roadshow'),
+                 ('roundtable', 'Roundtable'),
+                 ('speech', 'Speech'),
+                 ('visit', 'Visit'),
+                 ('webinar', 'Webinar')
+                 ],
+        max_length=100
+    )
+    country = CountryField(blank_label='(select country)', max_length=100)
+    region = models.CharField(max_length=100)
+    city_or_town = models.CharField(max_length=100)
     attending_organisations = models.TextField()
     attending_contacts = models.TextField()
 
     def __str__(self):
         return f"{self.interaction_date} {self.interaction_title}"
+
 
 class Csv(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
