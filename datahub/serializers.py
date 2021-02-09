d9075905f70081c9083315f97f56797b0c70b5fe
Chloe <chloe.wong@madetech.com>
2021-02-09 10:30:12 +0000

    Changed fields to __all__ to shorten code

--- datahub/serializers.py
+++ datahub/serializers.py
@@ -8,15 +8,4 @@ class BedSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BedTemplate
-        fields = (
-            'id',
-            'interaction_title',
-            'interaction_date',
-            'lead_minister_or_official',
-            'type',
-            'country',
-            'region',
-            'city_or_town',
-            'attending_organisations',
-            'attending_contacts',
-        )
+        fields = '__all__'
